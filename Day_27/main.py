c21ecdf88484e81be8d871fe267d692bf04dcd28
Fe <fe.my.projects@gmail.com>
2024-10-22 14:39:29 -0300

    Desenvolvimento concluido
    Inicie o "main.py"
    Ao salvar, será criado um arquivo.json
    
    Algumas atualizaçÕes:
    - Dados são salvos em JSON.
    - Adicionado o botão "Procurar"
    Retorna o email e senha caso estejam armazenados.

--- Day_27/main.py
+++ Day_27/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 #Carrega os dados salvos no saved_email.txt
 with open('100days_python/Day_27/saved_email/saved_email.txt', 'r') as data_file:
@@ -27,26 +28,52 @@ def gen_password():
     pyperclip.copy(password)
     
 
-# ---------------------------- SALVA DADOS ------------------------------- #
+# ---------------------------- ARMAZENA DADOS ------------------------------- #
 def save_data():
     #Pega e armazena os dados enviados em uma tupla
     data_tuple = (site_entry, password_entry)
+    new_data_dict = {data_tuple[0].get().lower(): {
+                    "Email": email_user_entry.get(),
+                    "Password": data_tuple[1].get()
+    }}
+
     #Valida se todos os campos do formulário foram preenchidos
     if len(data_tuple[0].get()) == 0 or len(email_user_entry.get()) == 0 or len(data_tuple[1].get()) == 0:
         messagebox.showwarning(title='Oops...', message='Por favor, não deixe nenhum campo vazio.')
     else:
-        #Pede a confirmação do usuário se deseja salvar os dados inseridos
-        is_ok = messagebox.askokcancel(title=f'{data_tuple[0].get()}', message=f'Dados à serem salvos: \nSite: {data_tuple[0].get()} \nEmail/Username: {email_user_entry.get()} \nSenha: {data_tuple[1].get()} \nDeseja prosseguir com os dados fornecidos?')
-        #Caso seja confirmado, procede em salvar
-        if is_ok:
-            #Cria e atualiza um arquivo txt com os dados enviados
-            with open('100days_python/Day_27/saved_password.txt', 'a') as data_file:
-                data_file.write(f"Website: {data_tuple[0].get()} | Email/Username: {email_user_entry.get()} | Senha: {data_tuple[1].get()}\n")
-            with open('100days_python/Day_27/saved_email/saved_email.txt', 'w') as saved_data_file:
-                saved_data_file.write(email_user_entry.get())
-            #Após adicionar os dados no arquivo, faz um loop deletando os dados no campo do formulário
-            for data in data_tuple:
-                data.delete(0, END)
+        #Atualiza o arquivo JSON caso exista
+        try:
+            #Carrega e atualiza o antigo arquivo JSON
+            with open('100days_python/Day_27/saved_password.json', 'r') as data_file:
+                json_data = json.load(data_file)
+                json_data.update(new_data_dict)    
+            
+            #Armazena os dados em JSON
+            with open('100days_python/Day_27/saved_password.json', 'w') as data_file:
+                json.dump(json_data, data_file, indent=4)
+        
+        #Cria um arquivo JSON caso não exista
+        except FileNotFoundError:
+            with open('100days_python/Day_27/saved_password.json', 'w') as data_file:
+                json.dump(new_data_dict, data_file, indent=4)
+
+        #Atualiza e salva o email no campo
+        with open('100days_python/Day_27/saved_email/saved_email.txt', 'w') as saved_data_file:
+            saved_data_file.write(email_user_entry.get())
+        
+        #Após adicionar os dados no arquivo, faz um loop deletando os dados no campo do formulário
+        for data in data_tuple:
+            data.delete(0, END)
+
+# ---------------------------- FUNÇÃO DE PROCURA ------------------------------- #
+def search_data():
+    search_entry = site_entry.get()
+    try:
+        with open('100days_python/Day_27/saved_password.json', 'r') as data_file:
+            json_data = json.load(data_file)
+            messagebox.showinfo(title=search_entry, message=f'Email/Username: {json_data[search_entry.lower()]['Email']} \nSenha: {json_data[search_entry.lower()]['Password']}')
+    except (KeyError, FileNotFoundError):
+            messagebox.showwarning(title='Erro', message=f'Nenhum dado do site "{search_entry}" foi encontrado.')
 
 # ---------------------------- CONFIGURAÇÃO UI ------------------------------- #
 
@@ -73,7 +100,7 @@ password_label.grid(column=0, row=3)
 
 #Entries
 site_entry = Entry()
-site_entry.grid(column=1, row=1, columnspan=2, sticky='EW')
+site_entry.grid(column=1, row=1, sticky='EW')
 site_entry.focus()
 
 email_user_entry = Entry()
@@ -84,10 +111,13 @@ password_entry = Entry()
 password_entry.grid(column=1, row=3, sticky='EW')
 
 #Botões
-pass_gen_buttton = Button(text='Gerar senha', highlightthickness=0, command=gen_password)
+search_button = Button(text='Procurar', command=search_data)
+search_button.grid(column=2, row=1, sticky='EW')
+
+pass_gen_buttton = Button(text='Gerar senha', command=gen_password)
 pass_gen_buttton.grid(column=2, row=3)
 
-add_button = Button(text='Adicionar', highlightthickness=0, command=save_data)
+add_button = Button(text='Adicionar', command=save_data)
 add_button.grid(column=1, row=4, columnspan=2, sticky='EW')
 
 #Loop para manter janela aberta
