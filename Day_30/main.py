a8b3bd3833760816a74ff1e24d02c5e8919a6005
Fe <fe.my.projects@gmail.com>
2024-10-30 12:23:01 -0300

    Desenvolvimento concluido.
    Configure as informações no arquivo "config.py"
    E inicie o main.py
    A cada 1 minuto, o programa deve verificar se a ISS está por perto ou não.

--- Day_30/main.py
+++ Day_30/main.py
@@ -30,7 +30,7 @@ def is_night_time():
     "lng": MY_LONG,
     "formatted": 0,
     }
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response = requests.get("http://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
 
